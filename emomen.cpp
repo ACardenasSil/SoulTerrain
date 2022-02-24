60d24c7a4aabf24022c7368cee269fde3ca82664
Evan Momen <emomen@odin.cs.csubak.edu>
2022-02-24 12:26:52 -0800

    minor fix

--- emomen.cpp
+++ emomen.cpp
@@ -2,7 +2,7 @@
 // Author: Evan Momen
 // Date: 2022
 
-void drawHealthBar(Rect r) {
+void drawHealthBar() {
     //ggprint8b(&r, 16, 0x00ffff00, "HEALTH BAR HERE");
     printf("HEALTH BAR HERE");
 }
